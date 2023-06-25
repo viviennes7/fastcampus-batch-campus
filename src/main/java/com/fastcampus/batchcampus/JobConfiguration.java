f8f01de11a4a10b852e0a35bb43c8879043731e0
Minsoo Kim <viviennes7@naver.com>
2023-06-25 15:19:38 +0900

    2-2 Job

--- src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
+++ src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
@@ -1,9 +1,12 @@
 package com.fastcampus.batchcampus;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.core.annotation.BeforeJob;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -19,6 +22,10 @@ public class JobConfiguration {
     public Job job(JobRepository jobRepository, Step step) {
         return new JobBuilder("job", jobRepository)
                 .start(step)
+                .preventRestart()
+                .incrementer(new RunIdIncrementer())
+                .validator(new DefaultJobParametersValidator(new String[]{"name"}, new String[]{"age"}))
+                .listener(new AnnotationJobExecutionListener())
                 .build();
     }
 
@@ -26,10 +33,54 @@ public class JobConfiguration {
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("step", jobRepository)
                 .tasklet((a, b) -> {
-                    log.info("step 실패");
+                    log.info("step 실행");
                     return RepeatStatus.FINISHED;
                 }, platformTransactionManager)
                 .build();
+    }
+
+    public static class CustomJobParametersValidator implements JobParametersValidator {
+
+        @Override
+        public void validate(JobParameters parameters) throws JobParametersInvalidException {
+            if (parameters.getString("name") == null) {
+                throw new JobParametersInvalidException("name이 필요해요!");
+            }
+        }
+
+    }
+
+    @Slf4j
+    public static class CustomJobExecutionListener implements JobExecutionListener {
+
+        @Override
+        public void beforeJob(JobExecution jobExecution) {
+            log.info("beforeJob 입니다");
+            JobExecutionListener.super.beforeJob(jobExecution);
+        }
+
+        @Override
+        public void afterJob(JobExecution jobExecution) {
+            if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+                log.info("afterJob 입니다.");
+                JobExecutionListener.super.afterJob(jobExecution);
+            }
+        }
+    }
+
+    public static class AnnotationJobExecutionListener {
+
+        @BeforeJob
+        public void before() {
+            log.info("annotation before 입니다.");
+
+        }
+
+        @AfterJob
+        public void after() {
+            log.info("annotation after 입니다.");
+        }
+
     }
 
 }
