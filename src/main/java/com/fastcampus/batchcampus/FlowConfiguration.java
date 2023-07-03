a5f8dcc0c035079f1c3f00d4fb926ecf46fd5747
Minsoo Kim <viviennes7@naver.com>
2023-07-03 14:30:06 +0900

    2-4 ItemReader

--- src/main/java/com/fastcampus/batchcampus/FlowConfiguration.java
+++ src/main/java/com/fastcampus/batchcampus/FlowConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
-@Configuration
+//@Configuration
 public class FlowConfiguration {
 
     @Bean
---
+++ src/main/java/com/fastcampus/batchcampus/User.java
@@ -0,0 +1,23 @@
+package com.fastcampus.batchcampus;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "USER")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String name;
+
+    private String age;
+
+    private String region;
+
+    private String telephone;
+
+}
