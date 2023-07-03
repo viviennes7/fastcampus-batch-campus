339bfa6b6514a638187abe63078d8c7e0d2ce75e
Minsoo Kim <viviennes7@naver.com>
2023-07-03 11:28:10 +0900

    2-3 Step

---
+++ src/main/java/com/fastcampus/batchcampus/FlowConfiguration.java
@@ -0,0 +1,62 @@
+package com.fastcampus.batchcampus;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+public class FlowConfiguration {
+
+    @Bean
+    public Job flowJob(
+            JobRepository jobRepository,
+            Step step1,
+            Step step2,
+            Step step3
+    ) {
+        return new JobBuilder("flowJob", jobRepository)
+                .start(step1)
+                    .on("COMPLETED").stopAndRestart(step2)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((a, b) -> {
+                    log.info("step1 실행");
+                    return null;
+                }, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((a, b) -> {
+                    log.info("step2 실행");
+                    return null;
+                }, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet((a, b) -> {
+                    log.info("step3 실행");
+                    return null;
+                }, platformTransactionManager)
+                .build();
+    }
+
+
+}
--- src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
+++ src/main/java/com/fastcampus/batchcampus/JobConfiguration.java
@@ -1,86 +1,91 @@
 package com.fastcampus.batchcampus;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.annotation.AfterJob;
-import org.springframework.batch.core.annotation.BeforeJob;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
-@Configuration
+//@Configuration
 public class JobConfiguration {
 
     @Bean
     public Job job(JobRepository jobRepository, Step step) {
-        return new JobBuilder("job", jobRepository)
+        return new JobBuilder("job-chunk", jobRepository)
                 .start(step)
-                .preventRestart()
-                .incrementer(new RunIdIncrementer())
-                .validator(new DefaultJobParametersValidator(new String[]{"name"}, new String[]{"age"}))
-                .listener(new AnnotationJobExecutionListener())
                 .build();
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+    @JobScope
+    public Step step(
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager,
+            @Value("#{jobParameters['name']}") String name
+    ) {
+        log.info("name : {}", name);
         return new StepBuilder("step", jobRepository)
-                .tasklet((a, b) -> {
-                    log.info("step 실행");
-                    return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
+                .tasklet((a, b) -> RepeatStatus.FINISHED, platformTransactionManager)
                 .build();
     }
 
-    public static class CustomJobParametersValidator implements JobParametersValidator {
+    /*@Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
 
-        @Override
-        public void validate(JobParameters parameters) throws JobParametersInvalidException {
-            if (parameters.getString("name") == null) {
-                throw new JobParametersInvalidException("name이 필요해요!");
+        final ItemReader<Integer> itemReader = new ItemReader<>() {
+
+            private int count = 0;
+
+            @Override
+            public Integer read() {
+                count++;
+
+                log.info("Read {}", count);
+
+                if (count == 20)
+                    return null;
+
+//                if (count >= 15)
+//                    throw new IllegalStateException("예외가 발생했어요.");
+
+                return count;
+
             }
-        }
+        };
 
-    }
+        final ItemProcessor<Integer, Integer> itemProcessor = new ItemProcessor<>() {
+            @Override
+            public Integer process(Integer item) throws Exception {
 
-    @Slf4j
-    public static class CustomJobExecutionListener implements JobExecutionListener {
+                if(item == 15)
+                    throw new IllegalStateException();
 
-        @Override
-        public void beforeJob(JobExecution jobExecution) {
-            log.info("beforeJob 입니다");
-            JobExecutionListener.super.beforeJob(jobExecution);
-        }
-
-        @Override
-        public void afterJob(JobExecution jobExecution) {
-            if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-                log.info("afterJob 입니다.");
-                JobExecutionListener.super.afterJob(jobExecution);
+                return item;
             }
-        }
-    }
+        };
 
-    public static class AnnotationJobExecutionListener {
+        return new StepBuilder("step", jobRepository)
+                .<Integer, Integer>chunk(10, platformTransactionManager)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(read -> {
+                })
+                .faultTolerant()
+                .retry(IllegalStateException.class)
+                .retryLimit(5)
+                .build();
+    }*/
 
-        @BeforeJob
-        public void before() {
-            log.info("annotation before 입니다.");
-
-        }
-
-        @AfterJob
-        public void after() {
-            log.info("annotation after 입니다.");
-        }
-
-    }
+    // 1~ 10 commit 11~15
+    //    1,2,3,4,5 rollback
+    // 11~15 rollback
 
 }
